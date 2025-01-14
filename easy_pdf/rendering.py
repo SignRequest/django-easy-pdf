e3b98e69d536ad783ce9bf19e75be38ab6813219
Andrei Visan <avisan@box.com>
2025-01-14 10:56:43 +0100

    Refactor django.utils.http.urlquote to urllib.parse.quote

--- easy_pdf/rendering.py
+++ easy_pdf/rendering.py
@@ -12,7 +12,7 @@ except ImportError:
 from django.core.files.base import ContentFile
 from django.http import HttpResponse, HttpRequest
 from django.template import loader
-from django.utils.http import urlquote
+from urllib.parse import quote
 from weasyprint import CSS, HTML, default_url_fetcher
 
 __all__ = [
@@ -82,7 +82,7 @@ def encode_filename(filename):  # type: (Text) -> Text
     # TODO: http://greenbytes.de/tech/webdav/rfc6266.html
     # TODO: http://greenbytes.de/tech/tc2231/
 
-    quoted = urlquote(filename)
+    quoted = quote(filename)
     if quoted == filename:
         return "filename=%s" % filename
     else:
